249ba4058673e02ae501db7a1400cb6ee77aa373
VatsalSoni_13 <sonivatsal2001@gmail.com>
2021-03-19 09:50:47 +0530

    Calendar setUp

--- navigation/BatchPanel.js
+++ navigation/BatchPanel.js
@@ -3,6 +3,7 @@ import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs
 const Tab = createMaterialTopTabNavigator();
 import BatchInfo from '../screens/Tutor/BatchInfo';
 import Chat from '../screens/Tutor/Chat';
+import Calendar from '../screens/Tutor/Calendar';
 import Material from '../screens/Tutor/Material';
 import Assignments from '../screens/Tutor/Assignments';
 export const NetworkContext = createContext();
@@ -13,7 +14,9 @@ const BatchPanel = ({route}) => {
         <Tab.Screen name="Chat" component={Chat} />
         <Tab.Screen name="Material" component={Material} />
         <Tab.Screen name="Assignments" component={Assignments} />
-        <Tab.Screen name="Info" component={BatchInfo} />
+        {/* Give an 'i' button for Batch Info */}
+        {/* <Tab.Screen name="Info" component={BatchInfo} />  */}
+        <Tab.Screen name="Calendar" component={Calendar} />
       </Tab.Navigator>
     </NetworkContext.Provider>
   );
--- screens/Tutor/Calendar.js
+++ screens/Tutor/Calendar.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {
   View,
   Text,
@@ -7,17 +7,59 @@ import {
   Platform,
   TouchableOpacity,
   Alert,
+  Dimensions,
+  Linking,
 } from 'react-native';
-const Calendar = () => {
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import {NetworkContext} from '../../navigation/BatchPanel';
+const Calendar = ({navigation}) => {
+  const batchId = useContext(NetworkContext);
+
   return (
-    <View>
-      <Text>Calendar</Text>
+    <View style={styles.container}>
+      <View>
+        <Text>Calendar</Text>
+      </View>
+      <View style={styles.floatButton}>
+        <TouchableOpacity
+          onPress={() => {
+            console.log(batchId);
+            Linking.openURL(
+              `https://gcalendar-intuition.herokuapp.com/schedule/${batchId}`,
+            )
+              .then(() => {
+                console.log('Success');
+              })
+              .catch((e) => {
+                console.log(e);
+              });
+            // navigation.navigate('Gcalendar', {
+            //   batchId: batchId,
+            // });
+          }}>
+          <FontAwesome name="calendar-plus-o" size={30} color="#fff" />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  floatButton: {
+    borderWidth: 1,
+    borderColor: 'rgba(0,0,0,0.2)',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 70,
+    height: 70,
+    position: 'absolute',
+    top: Dimensions.get('screen').height / 2 + 130,
+    right: 20,
+    opacity: 0.9,
+    backgroundColor: '#B83227',
+    borderRadius: 100,
   },
 });
 
